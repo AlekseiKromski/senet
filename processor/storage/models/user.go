02265f3059338e5a651f2143c71083ec18b061f9
alekseiKromski <aleksei.kromski@outlook.com>
2023-09-10 15:44:55 +0300

    models: extend models for more proper returns

--- processor/storage/models/user.go
+++ processor/storage/models/user.go
@@ -4,8 +4,9 @@ type User struct {
 	ID         string  `json:"id"`
 	Username   string  `json:"username"`
 	Password   *string `json:"password,omitempty"` //null for safety
+	Image      *string `json:"image"`              // null
 	LastOnline string  `json:"lastOnline"`
-	CreatedAt  string  `json:"createdAt"`
-	UpdatedAt  string  `json:"updatedAt"`
-	DeletedAt  *string `json:"deletedAt"` // null
+	CreatedAt  string  `json:"createdAt,omitempty"` //null
+	UpdatedAt  string  `json:"updatedAt,omitempty"` //null
+	DeletedAt  *string `json:"deletedAt,omitempty"` // null
 }
